61259770f906c37f0a05fe2317f5798b2fa3c8a6
Jaqobel <me.jakub.gil@gmail.com>
2023-07-31 18:31:19 +0200

    First animation

--- mainApp.py
+++ mainApp.py
@@ -1,19 +1,20 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.animation import Animation
 from kivy.lang.builder import Builder
 
 Builder.load_file('./main.kv')
 
-class background(Widget):
-    pass
 
 class welcomePage(Widget):
     background_color = (253/255, 240/255, 213/255, 1)
     text_color = (240/255, 240/255, 240/255, 1)
     button_color = (0, 48/255, 73/255, 1)
     warning_color = (193/255, 18/255, 31/255, 1)
-    background = background()
-    pass
+
+    def login_press(self, widget, *args):
+        animate = Animation(opacity=0, duration=0.7)
+        animate.start(widget)
 
 class mainWindow(App):
     def build(self):
