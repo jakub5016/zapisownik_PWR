45eeedec59437d61f3cb920fc327d160fcdfc205
Jaqobel <me.jakub.gil@gmail.com>
2023-08-02 21:37:34 +0200

    Refactor

--- mainApp.py
+++ mainApp.py
@@ -5,20 +5,16 @@ from kivy.lang.builder import Builder
 
 Builder.load_file('./main.kv')
 
+class rootClass(Widget):
+    background_color = (253/255, 240/255, 213/255, 1)
+    pass
 
 class welcomePage(Widget):
-    background_color = (253/255, 240/255, 213/255, 1)
-    text_color = (240/255, 240/255, 240/255, 1)
-    button_color = (0, 48/255, 73/255, 1)
-    warning_color = (193/255, 18/255, 31/255, 1)
-
-    def login_press(self, widget, *args):
-        animate = Animation(opacity=0, duration=0.7)
-        animate.start(widget)
+    pass
 
 class mainWindow(App):
     def build(self):
-        return welcomePage()
+        return rootClass()
 
 
 if __name__ == '__main__':
